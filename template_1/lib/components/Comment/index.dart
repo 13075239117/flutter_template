ffeb2dc2b4435af5b5851842652ce1729ebb3771
小欧 <935764227@qq.com>
2022-10-24 17:55:18 +0800

    feat:增加弹框

--- template_1/lib/components/Comment/index.dart
+++ template_1/lib/components/Comment/index.dart
@@ -27,21 +27,43 @@ class _CommmentState extends State<Commment> {
             child: ListView.builder(
               itemCount: 21,
               itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.red,
-                    // backgroundImage:
-                    //     NetworkImage(_listData[widget.index].avatar),
-                    // backgroundColor: Colors.brown.shade800,
-                    child: Text("测试", textAlign: TextAlign.center),
-                    radius: 15,
+                return GestureDetector(
+                  onTap: () {
+                    OverlayState? state = Overlay.of(context);
+                    OverlayEntry overlayEntry =
+                        OverlayEntry(builder: (context) {
+                      return Material(
+                        type: MaterialType.transparency,
+                        child: SizedBox(
+                          width: 200,
+                          height: 100,
+                          child: Center(
+                              child: TextField(
+                            decoration: InputDecoration(
+                              icon: Icon(Icons.person),
+                            ),
+                          )),
+                        ),
+                      );
+                    });
+                    state?.insert(overlayEntry);
+                  },
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.red,
+                      // backgroundImage:
+                      //     NetworkImage(_listData[widget.index].avatar),
+                      // backgroundColor: Colors.brown.shade800,
+                      child: Text("测试", textAlign: TextAlign.center),
+                      radius: 15,
+                    ),
+                    title: Text("咩咩阿三大苏打实打实大苏打实打实"),
+                    subtitle: Text(
+                      "2022-10-19",
+                      textAlign: TextAlign.right,
+                    ),
+                    trailing: Text("123123"),
                   ),
-                  title: Text("咩咩阿三大苏打实打实大苏打实打实"),
-                  subtitle: Text(
-                    "2022-10-19",
-                    textAlign: TextAlign.right,
-                  ),
-                  trailing: Text("123123"),
                 );
               },
             ),
